7bdc20a60513064ca76f875c8ed9fdbc6a657b76
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-27 04:27:12 +0800

    Add function __init__, __str__ and import

--- placecollection.py
+++ placecollection.py
@@ -1,9 +1,22 @@
-"""..."""
-
-
+"""
+CP1404 Assignment 2
+class for multiple places
+"""
+import csv
+from operator import itemgetter
 # Create your PlaceCollection class in this file
 
 
 class PlaceCollection:
-    """..."""
-    pass
+    """Represent operations to multiple places"""
+
+    def __init__(self):
+        self.places_list = []
+
+    def __str__(self):
+        output = ''
+        for perplace in self.places_list:
+            for i in perplace[:-1]:
+                output += str(i) + ','
+            output = output + '\n'
+        return output
