06807a29486928e7bda37b3ac22ad6c1f153f149
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-27 04:27:41 +0800

    Add function load_places

--- placecollection.py
+++ placecollection.py
@@ -20,3 +20,16 @@ class PlaceCollection:
                 output += str(i) + ','
             output = output + '\n'
         return output
+
+    def load_places(self, filename):
+        """Return a list of unvisited and visited places"""
+
+        # Read file, and add data in dictionary
+        try:
+            with open(filename, 'r') as in_file:
+                places_detail = in_file.readlines()
+                for perplace in places_detail:
+                    perplace = perplace.strip().split(',')
+                    self.places_list.append(perplace)
+                for i in self.places_list:
+                    i[2] = int(i[2])
