2b34439389f153e53972cc390e999fc6b070c751
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-27 04:35:06 +0800

    Add import and main function

--- a1_classes.py
+++ a1_classes.py
@@ -1,5 +1,37 @@
-"""..."""
+"""
+CP1404 Assignment 2
+copy of Assignment 1
+"""
 # Copy your first assignment to this file, then update it to use Place class
 # Optionally, you may also use PlaceCollection class
 
 from place import Place
+from placecollection import PlaceCollection
+
+def main():
+    """Run the whole process"""
+    print("Travel Tracker 1.0 - by <Yudan Zhang>")
+    place_collection = PlaceCollection()
+
+    placs_list = place_collection.load_places('places.csv')
+
+    while True:
+        choice = read_menu()
+        if choice == 'L':
+            place_collection.list_places()
+
+        elif choice == 'A':
+            new_place = add_places()
+            place_collection.add_place(Place(new_place[0], new_place[1], new_place[2], new_place[3]))
+
+        elif choice == 'M':
+            mark_places(place_collection, placs_list)
+
+        elif choice == 'Q':
+            print("""{} places saved to places.csv
+Have a nice day :)""".format(len(place_collection.places_list)))
+            break
+
+        else:
+            print("Invalid menu choice")
+
