35d9bba23f49a5790c093c4094fd55291003905d
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-27 04:42:26 +0800

    Import necessary methods

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 Name: Yudan Zhang
 Date: 24/9/2020
 Brief Project Description: an application to add new place and mark visited and unvisited places.
-GitHub URL:
+GitHub URL:https://github.com/JCUS-CP1404/assignment-2-travel-tracker-ZHANGYUDAN1222
 """
 # Create your main program in this file, using the TravelTrackerApp class
 
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+from kivy.properties import ListProperty
 
+from placecollection import PlaceCollection
 
 class TravelTrackerApp(App):
     """..."""
