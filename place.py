d901657beeb5872191119c72354951bd318ab880
ZHANGYUDAN1222 <1009040705@qq.com>
2020-09-27 04:20:58 +0800

    Add funvtion __ init__

--- place.py
+++ place.py
@@ -1,9 +1,22 @@
-"""..."""
-
+"""
+1404 Assignment
+class for functions of each place
+"""
 
 # Create your Place class in this file
 
 
 class Place:
-    """..."""
-    pass
+    """Represent a Place object"""
+
+    def __init__(self, name='', country='', priority=0, v_status=''):
+        """Initialise a place instance"""
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.v_status = v_status
+
+        if self.v_status == False:
+            self.v_status = 'n'
+        elif self.v_status == True:
+            self.v_status = 'v'
